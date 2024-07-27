0319741e8fc746ab6169dceebc973ab89145a029
jeroen <jpjagt@pm.me>
2024-07-27 19:55:12 +0200

    add pomegranate lyrics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,20 @@
 import { defineConfig } from 'astro/config';
 import path from 'path';
-
 import tailwind from "@astrojs/tailwind";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), alpinejs()],
   site: 'https://july.dev',
   // base: '/july.dev',
   vite: {
     resolve: {
       alias: {
         // Set up '@' to point to the 'src' directory
-        '@': path.resolve('./src'),
-      },
-    },
-  },
+        '@': path.resolve('./src')
+      }
+    }
+  }
 });
